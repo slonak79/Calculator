328a3a7d0e5e408fb68fd4085c2a8c99171767e7
Alcides Sorto <alsorto@gmail.com>
2015-09-15 21:41:36 -0700

    implemented requirements for assignment 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,35 +7,63 @@
 //
 
 import UIKit
+import Darwin
 
 class ViewController: UIViewController {
 
+    let pi = M_PI
+    
     @IBOutlet weak var display: UILabel!
+    
+    
+    
+    @IBOutlet weak var operandDisplay: UILabel!
+    
+    
+    
     
     var userIsInTheMiddleOfTypingANumber: Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
         
         let digit = sender.currentTitle!
+  
         
         
-        if display.text?.rangeOfString(".") == nil{
         
-            display.text = display.text! + digit
-            
-        }
         
-        else if userIsInTheMiddleOfTypingANumber {
+        
+        
+
+        if userIsInTheMiddleOfTypingANumber {
             
-            display.text = display.text! + digit
-        }
-        else{
+            var canEnterDecimal = true
             
-            display.text = digit
             
-            userIsInTheMiddleOfTypingANumber = true
             
-        }
+                if digit == "π"{
+                    println(pi.description)
+                    operandDisplay.text = operandDisplay.text! + " " +  digit
+                    
+                }else{
+                    
+                    if digit == "." && display.text?.rangeOfString(".") != nil{canEnterDecimal=false}
+                    
+                    if canEnterDecimal{
+                        display.text = display.text! + digit
+                        operandDisplay.text = operandDisplay.text! + " " +  digit
+                    }
+                }
+            
+            
+            }
+            else{
+                operandDisplay.text = operandDisplay.text! + " " +  digit
+                display.text = digit
+            
+                userIsInTheMiddleOfTypingANumber = true
+            
+            }
         
     }
     
@@ -45,16 +73,32 @@ class ViewController: UIViewController {
             enter()
         }
         switch operation{
+            
             case "×":performOperation { $0 * $1}
+                    operandDisplay.text = operandDisplay.text! + " " + "x"
             case "−":performOperation { $0 - $1}
+                    operandDisplay.text = operandDisplay.text! + " " + "-"
             case "+":performOperation { $0 + $1}
+                    operandDisplay.text = operandDisplay.text! + " " + "+"
             case "÷":performOperation { $0 / $1}
+                    operandDisplay.text = operandDisplay.text! + " " + "÷"
             case "√":performOperation2 { sqrt($0)}
+                    operandDisplay.text = operandDisplay.text! + " " + "√"
+            case "sin": performOperation2 { sin($0) }
+                        operandDisplay.text = operandDisplay.text! + " " + "sin"
+            
+            case "cos": performOperation2 { cos($0) }
+                        operandDisplay.text = operandDisplay.text! + " " + "cos"
+        
+            
+            
             default: break
             
             
         }
     }
+    
+    
     
     func performOperation(operation: (Double, Double) -> Double){
         if operandStack.count >= 2{
@@ -76,7 +120,10 @@ class ViewController: UIViewController {
     @IBAction func enter() {
         
         userIsInTheMiddleOfTypingANumber = false
+        
+        
         operandStack.append(displayValue)
+        
         println("operandStack = \(operandStack)")
         
         
@@ -84,12 +131,25 @@ class ViewController: UIViewController {
     
     var displayValue: Double{
         get{
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            if display.text! != "π"{
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }else{
+                return pi
+            }
         }
         set{
             display.text = "\(newValue)"
             userIsInTheMiddleOfTypingANumber = false
         }
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        
+        display.text = "0"
+        operandDisplay.text = ""
+        operandStack.removeAll()
+        println("operandStack = \(operandStack)")
+        userIsInTheMiddleOfTypingANumber = false
     }
 
 }
